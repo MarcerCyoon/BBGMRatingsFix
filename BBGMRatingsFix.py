4302c32ecb57c1a5a0ee9833bea3a2ef2483a320
MarcerCyoon <4866635+MarcerCyoon@users.noreply.github.com>
2019-12-15 22:20:14 -0500

    Fixed bug; now properly selects latest season

--- BBGMRatingsFix.py
+++ BBGMRatingsFix.py
@@ -39,7 +39,6 @@ rebChange = 3
 
 # Change Details to Accomodate Your File
 fileName = "fixExport.json"
-currentSeason = 2025
 
 # Open File
 with open(fileName, 'r', encoding='utf-8-sig') as file:
@@ -48,30 +47,30 @@ with open(fileName, 'r', encoding='utf-8-sig') as file:
 players = export['players']
 
 print(players[56]['firstName'] + " " + players[56]['lastName'])
-print(players[56]['ratings']['season' == currentSeason])
+print(players[56]['ratings'][-1])
 
 for player in players:
 	if (player['tid'] == -3):
 		continue
 	else:
 		player['hgt'] = max(0, player['hgt'] + hgtChange)
-		player['ratings']['season' == currentSeason]['hgt'] = max(0, player['ratings']['season' == currentSeason]['hgt'] + hgtChange)
-		player['ratings']['season' == currentSeason]['stre'] = max(0, streChange + player['ratings']['season' == currentSeason]['stre'])
-		player['ratings']['season' == currentSeason]['spd'] = max(0, spdChange + player['ratings']['season' == currentSeason]['spd'])
-		player['ratings']['season' == currentSeason]['jmp'] = max(0, jmpChange + player['ratings']['season' == currentSeason]['jmp'])
-		player['ratings']['season' == currentSeason]['endu'] = max(0, enduChange + player['ratings']['season' == currentSeason]['endu'])
-		player['ratings']['season' == currentSeason]['ins'] = max(0, insChange + player['ratings']['season' == currentSeason]['ins'])
-		player['ratings']['season' == currentSeason]['dnk'] = max(0, dnkChange + player['ratings']['season' == currentSeason]['dnk'])
-		player['ratings']['season' == currentSeason]['ft'] = max(0, ftChange + player['ratings']['season' == currentSeason]['ft'])
-		player['ratings']['season' == currentSeason]['fg'] = max(0, fgChange + player['ratings']['season' == currentSeason]['fg'])
-		player['ratings']['season' == currentSeason]['tp'] = max(0, tpChange + player['ratings']['season' == currentSeason]['tp'])
-		player['ratings']['season' == currentSeason]['diq'] = max(0, diqChange + player['ratings']['season' == currentSeason]['diq'])
-		player['ratings']['season' == currentSeason]['oiq'] = max(0, oiqChange + player['ratings']['season' == currentSeason]['oiq'])
-		player['ratings']['season' == currentSeason]['drb'] = max(0, drbChange + player['ratings']['season' == currentSeason]['drb'])
-		player['ratings']['season' == currentSeason]['pss'] = max(0, pssChange + player['ratings']['season' == currentSeason]['pss'])
-		player['ratings']['season' == currentSeason]['reb'] = max(0, rebChange + player['ratings']['season' == currentSeason]['reb'])
+		player['ratings'][-1]['hgt'] = max(0, player['ratings'][-1]['hgt'] + hgtChange)
+		player['ratings'][-1]['stre'] = max(0, streChange + player['ratings'][-1]['stre'])
+		player['ratings'][-1]['spd'] = max(0, spdChange + player['ratings'][-1]['spd'])
+		player['ratings'][-1]['jmp'] = max(0, jmpChange + player['ratings'][-1]['jmp'])
+		player['ratings'][-1]['endu'] = max(0, enduChange + player['ratings'][-1]['endu'])
+		player['ratings'][-1]['ins'] = max(0, insChange + player['ratings'][-1]['ins'])
+		player['ratings'][-1]['dnk'] = max(0, dnkChange + player['ratings'][-1]['dnk'])
+		player['ratings'][-1]['ft'] = max(0, ftChange + player['ratings'][-1]['ft'])
+		player['ratings'][-1]['fg'] = max(0, fgChange + player['ratings'][-1]['fg'])
+		player['ratings'][-1]['tp'] = max(0, tpChange + player['ratings'][-1]['tp'])
+		player['ratings'][-1]['diq'] = max(0, diqChange + player['ratings'][-1]['diq'])
+		player['ratings'][-1]['oiq'] = max(0, oiqChange + player['ratings'][-1]['oiq'])
+		player['ratings'][-1]['drb'] = max(0, drbChange + player['ratings'][-1]['drb'])
+		player['ratings'][-1]['pss'] = max(0, pssChange + player['ratings'][-1]['pss'])
+		player['ratings'][-1]['reb'] = max(0, rebChange + player['ratings'][-1]['reb'])
 
-print(players[56]['ratings']['season' == currentSeason])
+print(players[56]['ratings'][-1])
 
 # Replace Export Data with Fixed Data
 export['players'] = players
